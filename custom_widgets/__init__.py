940e2f8901c768bc0f5188caa9a6d89b82bb4735
Mikhail <misha.markov-03@mail.ru>
2022-11-10 00:50:22 +0300

    Удалил лишние файлы

--- custom_widgets/__init__.py
+++ custom_widgets/__init__.py
@@ -2,5 +2,4 @@ from . import main_window
 from . import registra_window
 from . import registra_window
 from . import scroll_widget
-from . import search_filter_window
 from . import university_preview
